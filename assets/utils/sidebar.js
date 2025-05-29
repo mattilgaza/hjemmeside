67198e069c7717f4545a314f23a604c01057d910
Andreas Sandnes <Asandnes92@gmail.com>
2025-05-29 14:42:18 +0200

    added stories; fixed overlay bug

--- assets/utils/sidebar.js
+++ assets/utils/sidebar.js
@@ -1,10 +1,14 @@
 function showSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex'
+    const content = document.querySelector('.content');
+    content.style.visibility = 'hidden'
 }
 
 function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none'
+    const content = document.querySelector('.content');
+    content.style.visibility = 'visible'
 }
 
